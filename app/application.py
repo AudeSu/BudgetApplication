a3b31f2a8e08533dd68c6931644a982c1411cb80
AudeSu <aude.sustronck@gmail.com>
2023-12-26 21:32:29 +0100

    Methoden toevoegen + aanpassen

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 import sqlite3
+import main
 
 conn = sqlite3.connect("../data/budget.db")
 cursor = conn.cursor()
@@ -8,7 +9,7 @@ def create_database():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT,
+            username TEXT UNIQUE,
             email TEXT UNIQUE,
             password TEXT
         )
@@ -29,38 +30,55 @@ def create_database():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER,
             amount REAL,
-            category TEXT CHECK(category IN ('Groceries', 'Electricity', 'Entertainment', 'Rent', 'Dining', 'Shopping', 'Transportation', 'Medical', 'Subscription', 'Other')),
+            category TEXT CHECK(category IN ('Groceries', 'Electricity', 'Entertainment', 'Rent', 'Dining', 'Shopping', 
+            'Transportation', 'Medical', 'Subscription', 'Other')),
             description TEXT,
             FOREIGN KEY(user_id) REFERENCES Users(id)
         )
     """)
 
 
-def register_user(username, email, password):
-    cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, password))
+def register_user():
+    username = input("Enter your username: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                   (username, email, password))
     conn.commit()
+    print("User registered successfully!\n")
 
 
-def login_user(username, password):
-    cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+def login_user():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    cursor.execute("SELECT * FROM users WHERE username=? AND password=?",
+                   (username, password))
     user = cursor.fetchone()
-    return user
+    if user:
+        user_id = user[0]  # Wat wordt er met dit lijntje code gedaan? die nul hangt toch af van wie er ingelogd is? moet dat ni "id" zijn uit de user tabel?
+        main.logged_in_menu(user_id)
+    else:
+        print("Invalid username or password. Please try again.")
 
 
 def get_income_overview(user_id):
-    cursor.execute("SELECT SUM(amount) FROM incomes WHERE user_id=?", (user_id,))
+    cursor.execute("SELECT SUM(amount) FROM incomes WHERE user_id=?",
+                   (user_id,))
     total_income = cursor.fetchone()[0]
-    return total_income if total_income else 0.0
+
+    print("Total Income:", total_income if total_income else 0.0)
 
 
 def get_expense_overview(user_id):
-    cursor.execute("SELECT SUM(amount) FROM expenses WHERE user_id=?", (user_id,))
+    cursor.execute("SELECT SUM(amount) FROM expenses WHERE user_id=?",
+                   (user_id,))
     total_expense = cursor.fetchone()[0]
-    return total_expense if total_expense else 0.0
+    print("Total Expense:", total_expense if total_expense else 0.0)
 
 
 def get_expense_by_category(user_id):
-    cursor.execute("SELECT category, SUM(amount) FROM expenses WHERE user_id=? GROUP BY category", (user_id,))
+    cursor.execute("SELECT category, SUM(amount) FROM expenses WHERE user_id=? GROUP BY category",
+                   (user_id,))
     expense_by_category = cursor.fetchall()
     return expense_by_category
 
@@ -78,20 +96,44 @@ def add_expense(user_id, amount, category, description):
 
 
 def delete_income(income_id):
-    cursor.execute("DELETE FROM incomes WHERE id=?", (income_id,))
+    cursor.execute("DELETE FROM incomes WHERE id=?",
+                   (income_id,))
     conn.commit()
 
 
 def delete_expense(expense_id):
-    cursor.execute("DELETE FROM expenses WHERE id=?", (expense_id,))
+    cursor.execute("DELETE FROM expenses WHERE id=?",
+                   (expense_id,))
     conn.commit()
 
 
 def edit_income(income_id, amount, description):
-    cursor.execute("UPDATE incomes SET amount=?, description=? WHERE id=?", (amount, description, income_id))
+    cursor.execute("UPDATE incomes SET amount=?, description=? WHERE id=?",
+                   (amount, description, income_id))
     conn.commit()
 
 
 def edit_expense(expense_id, amount, category, description):
-    cursor.execute("UPDATE expenses SET amount=?, category=?, description=? WHERE id=?", (amount, category, description, expense_id))
+    cursor.execute("UPDATE expenses SET amount=?, category=?, description=? WHERE id=?",
+                   (amount, category, description, expense_id))
     conn.commit()
+
+
+def get_all_incomes(user_id):
+    cursor.execute("SELECT * FROM incomes WHERE user_id=?", (user_id,))
+    return cursor.fetchall()
+
+
+def get_all_expenses(user_id):
+    cursor.execute("SELECT * FROM expenses WHERE user_id=?", (user_id,))
+    return cursor.fetchall()
+
+
+def get_all_expenses_category(user_id):
+    cursor.execute("SELECT * FROM expenses WHERE user_id=?", (user_id,))
+    return cursor.fetchall()
+
+
+def get_expenses_for_category(user_id, category):
+    cursor.execute("SELECT * FROM expenses WHERE user_id=? AND category=?", (user_id, category))
+    return cursor.fetchall()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,163 @@
 import application
 
+
+def display_menu():
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+
+
+def main_menu():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            application.register_user()
+        elif choice == '2':
+            application.login_user()
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def logged_in_menu(user_id):
+    while True:
+        application.get_income_overview(user_id)
+        application.get_expense_overview(user_id)
+        view_expense_by_category(user_id)
+        print("1. View all incomes")
+        print("2. View all expenses")
+        print("3. View all expenses for a category")
+        print("4. Add income")
+        print("5. Add expense")
+        print("6. Edit income")
+        print("7. Edit expense")
+        print("8. Delete income")
+        print("9. Delete expense")
+        print("10. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            view_all_incomes(user_id)
+        elif choice == '2':
+            view_all_expenses(user_id)
+        elif choice == '3':
+            view_all_expenses_category(user_id)
+        elif choice == '4':
+            add_income(user_id)
+        elif choice == '5':
+            add_expense(user_id)
+        elif choice == '6':
+            edit_income(user_id)
+        elif choice == '7':
+            edit_expense(user_id)
+        elif choice == '8':
+            delete_income(user_id)
+        elif choice == '9':
+            delete_expense(user_id)
+        elif choice == '10':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def view_expense_by_category(user_id):
+    expense_by_category = application.get_expense_by_category(user_id)
+    print("Expense by Category:")
+    for category, amount in expense_by_category:
+        print(f"{category}: {amount}")
+
+
+def view_all_expenses_category(user_id):
+    # Get all unique categories for the user
+    categories = set(expense[3] for expense in application.get_all_expenses_category(user_id))
+
+    print("Expense Categories:")
+    for i, category in enumerate(categories, start=1):
+        print(f"{i}. {category}")
+
+    # Ask the user to choose a category
+    category_choice = input("Choose a category (enter the corresponding number): ")
+
+    try:
+        category_index = int(category_choice) - 1
+        chosen_category = list(categories)[category_index]
+        view_expenses_for_category(user_id, chosen_category)
+    except (ValueError, IndexError):
+        print("Invalid choice. Please try again.")
+
+
+def view_expenses_for_category(user_id, category):
+    expenses_for_category = application.get_expenses_for_category(user_id, category)
+    print(f"All Expenses for {category}:")
+    for expense in expenses_for_category:
+        print(expense)
+
+
+def add_income(user_id):
+    amount = float(input("Enter the income amount: "))
+    description = input("Enter a description for the income: ")
+    application.add_income(user_id, amount, description)
+    print("Income added successfully!\n")
+
+
+def add_expense(user_id):
+    amount = float(input("Enter the expense amount: "))
+    category = input("Enter the expense category: ")
+    description = input("Enter a description for the expense: ")
+    application.add_expense(user_id, amount, category, description)
+    print("Expense added successfully!\n")
+
+
+def view_all_incomes(user_id):
+    all_incomes = application.get_all_incomes(user_id)
+    print("All Incomes:")
+    for income in all_incomes:
+        print(income)
+
+
+def view_all_expenses(user_id):
+    all_expenses = application.get_all_expenses(user_id)
+    print("All Expenses:")
+    for expense in all_expenses:
+        print(expense)
+
+
+def view_all_expenses_category(user_id):
+    all_expenses_category = application.get_all_expenses_category(user_id)
+    print("All Expenses by Category:")
+    for expense in all_expenses_category:
+        print(expense)
+
+
+def edit_income(user_id):
+    view_all_incomes(user_id)
+
+    expense_id = input("Enter the ID of the income you want to delete: ")
+
+
+def edit_expense(user_id):
+    view_all_expenses(user_id)
+
+    expense_id = input("Enter the ID of the expense you want to edit: ")
+
+
+def delete_income(user_id):
+    view_all_incomes(user_id)
+
+    expense_id = input("Enter the ID of the income you want to delete: ")
+
+
+def delete_expense(user_id):
+    view_all_expenses(user_id)
+
+    expense_id = input("Enter the ID of the expense you want to delete: ")
+
+
 if __name__ == '__main__':
     application.create_database()
+    main_menu()
     application.conn.close()
