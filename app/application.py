414a2e8b5b7c3c9da3281d200f22463546a6c4bc
AudeSu <aude.sustronck@gmail.com>
2023-12-26 22:07:22 +0100

    Methoden toevoegen + aanpassen 2.0

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 import main
 
 conn = sqlite3.connect("../data/budget.db")
@@ -55,13 +56,15 @@ def login_user():
                    (username, password))
     user = cursor.fetchone()
     if user:
-        user_id = user[0]  # Wat wordt er met dit lijntje code gedaan? die nul hangt toch af van wie er ingelogd is? moet dat ni "id" zijn uit de user tabel?
+        # Wat wordt er met dit lijntje code gedaan? die nul hangt toch af van wie er ingelogd is?
+        # moet dat ni "id" zijn uit de user tabel?
+        user_id = user[0]
         main.logged_in_menu(user_id)
     else:
         print("Invalid username or password. Please try again.")
 
 
-def get_income_overview(user_id):
+def get_total_income(user_id):
     cursor.execute("SELECT SUM(amount) FROM incomes WHERE user_id=?",
                    (user_id,))
     total_income = cursor.fetchone()[0]
@@ -69,71 +72,154 @@ def get_income_overview(user_id):
     print("Total Income:", total_income if total_income else 0.0)
 
 
-def get_expense_overview(user_id):
+def get_total_expense(user_id):
     cursor.execute("SELECT SUM(amount) FROM expenses WHERE user_id=?",
                    (user_id,))
     total_expense = cursor.fetchone()[0]
     print("Total Expense:", total_expense if total_expense else 0.0)
 
 
-def get_expense_by_category(user_id):
+def get_total_expense_by_category(user_id):
     cursor.execute("SELECT category, SUM(amount) FROM expenses WHERE user_id=? GROUP BY category",
                    (user_id,))
     expense_by_category = cursor.fetchall()
-    return expense_by_category
+
+    print("Expense by Category:")
+    for category, amount in expense_by_category:
+        print(f"{category}: {amount}")
 
 
-def add_income(user_id, amount, description):
+def add_income(user_id):
+    amount = float(input("Enter the income amount: "))
+    description = input("Enter a description for the income: ")
     cursor.execute("INSERT INTO incomes (user_id, amount, description) VALUES (?, ?, ?)",
                    (user_id, amount, description))
     conn.commit()
+    print("Income added successfully!\n")
 
 
-def add_expense(user_id, amount, category, description):
+def add_expense(user_id):
+    amount = float(input("Enter the expense amount: "))
+    # TODO: je zou enkel maar een categorie mogen opgeven dat in de database vermeld werdt
+    category = input("Enter the expense category: ")
+    description = input("Enter a description for the expense: ")
     cursor.execute("INSERT INTO expenses (user_id, amount, category, description) VALUES (?, ?, ?, ?)",
                    (user_id, amount, category, description))
     conn.commit()
+    print("Expense added successfully!\n")
 
 
-def delete_income(income_id):
+def delete_income(user_id):
+    view_all_incomes(user_id)
+    income_id = input("Enter the ID of the income you want to delete: ")
     cursor.execute("DELETE FROM incomes WHERE id=?",
                    (income_id,))
     conn.commit()
+    print(f"Income with ID {income_id} deleted successfully.\n")
 
 
-def delete_expense(expense_id):
+def delete_expense(user_id):
+    view_all_incomes(user_id)
+    expense_id = input("Enter the ID of the expense you want to delete: ")
     cursor.execute("DELETE FROM expenses WHERE id=?",
                    (expense_id,))
     conn.commit()
+    print(f"Expense with ID {expense_id} deleted successfully.\n")
 
 
-def edit_income(income_id, amount, description):
+def edit_income(user_id):
+    view_all_incomes(user_id)
+    income_id = input("Enter the ID of the income you want to edit: ")
+    amount = float(input("Enter the new income amount: "))
+    description = input("Enter a new description for the income: ")
     cursor.execute("UPDATE incomes SET amount=?, description=? WHERE id=?",
                    (amount, description, income_id))
     conn.commit()
+    print(f"Income with ID {income_id} edited successfully.\n")
 
 
-def edit_expense(expense_id, amount, category, description):
+def edit_expense(user_id):
+    view_all_incomes(user_id)
+    expense_id = input("Enter the ID of the expense you want to edit: ")
+    amount = float(input("Enter the expense amount: "))
+    # TODO: je zou enkel maar een categorie mogen opgeven dat in de database vermeld werdt
+    category = input("Enter the expense category: ")
+    description = input("Enter a description for the expense: ")
     cursor.execute("UPDATE expenses SET amount=?, category=?, description=? WHERE id=?",
                    (amount, category, description, expense_id))
     conn.commit()
+    print(f"Expense with ID {expense_id} edited successfully.\n")
 
 
-def get_all_incomes(user_id):
-    cursor.execute("SELECT * FROM incomes WHERE user_id=?", (user_id,))
-    return cursor.fetchall()
+def view_all_incomes(user_id):
+    cursor.execute("SELECT id, amount, description FROM incomes WHERE user_id=?",
+                   (user_id,))
+    all_incomes = cursor.fetchall()
+
+    if not all_incomes:
+        print("No incomes found.")
+        return
+
+    print("All Incomes:")
+    for income in all_incomes:
+        income_id, amount, description = income
+        print(f"ID: {income_id}, Amount: {amount}, Description: {description}")
 
 
-def get_all_expenses(user_id):
-    cursor.execute("SELECT * FROM expenses WHERE user_id=?", (user_id,))
-    return cursor.fetchall()
+def view_all_expenses(user_id):
+    cursor.execute("SELECT id, amount, category, description FROM expenses WHERE user_id=?",
+                   (user_id,))
+    all_expenses = cursor.fetchall()
+
+    if not all_expenses:
+        print("No expenses found.")
+        return
+
+    print("All Incomes:")
+    for expense in all_expenses:
+        expense_id, amount, category, description = expense
+        print(f"ID: {expense_id}, Amount: {amount}, Category: {category}, Description: {description}")
 
 
-def get_all_expenses_category(user_id):
-    cursor.execute("SELECT * FROM expenses WHERE user_id=?", (user_id,))
-    return cursor.fetchall()
+def view_all_expenses_by_category(user_id):
+    cursor.execute("SELECT category, SUM(amount) FROM expenses WHERE user_id=? GROUP BY category",
+                   (user_id,))
+    all_expenses_by_category = cursor.fetchall()
+
+    if not all_expenses_by_category:
+        print("No expenses found.")
+        return
+
+    print("Expenses by Category:")
+    for category, total_amount in all_expenses_by_category:
+        print(f"{category}: {total_amount}")
 
 
-def get_expenses_for_category(user_id, category):
-    cursor.execute("SELECT * FROM expenses WHERE user_id=? AND category=?", (user_id, category))
-    return cursor.fetchall()
+def view_all_expenses_by_selected_category(user_id):
+    # Eerst alle verschillende categorieën afdrukken
+    cursor.execute("SELECT DISTINCT category FROM expenses WHERE user_id=?", (user_id,))
+    categories = [category[0] for category in cursor.fetchall()]
+
+    if not categories:
+        print("No expense categories found.")
+        return
+
+    print("Expense Categories:")
+    for category in categories:
+        print(category)
+
+    # Vraag aan de gebruiker van welke categorie hij/zij alle uitgaven wil zien
+    selected_category = input("Enter the category to view expenses: ")
+
+    # Alle uitgaven voor de geselecteerde categorie afdrukken
+    cursor.execute("SELECT id, amount, description FROM expenses WHERE user_id=? AND category=?",
+                   (user_id, selected_category))
+    expenses_for_category = cursor.fetchall()
+
+    if not expenses_for_category:
+        print(f"No expenses found for category: {selected_category}")
+        return
+
+    print(f"Expenses for Category {selected_category}:")
+    for expense_id, amount, description in expenses_for_category:
+        print(f"ID: {expense_id}, Amount: {amount}, Description: {description}")
--- app/main.py
+++ app/main.py
@@ -24,9 +24,9 @@ def main_menu():
 
 def logged_in_menu(user_id):
     while True:
-        application.get_income_overview(user_id)
-        application.get_expense_overview(user_id)
-        view_expense_by_category(user_id)
+        application.get_total_income(user_id)
+        application.get_total_expense(user_id)
+        application.get_total_expense_by_category(user_id)
         print("1. View all incomes")
         print("2. View all expenses")
         print("3. View all expenses for a category")
@@ -41,120 +41,62 @@ def logged_in_menu(user_id):
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            view_all_incomes(user_id)
+            application.view_all_incomes(user_id)
         elif choice == '2':
-            view_all_expenses(user_id)
+            application.view_all_expenses(user_id)
+            print("===============gesorteerd per category========")
+            application.view_all_expenses_by_category(user_id)
         elif choice == '3':
-            view_all_expenses_category(user_id)
+            application.view_all_expenses_by_selected_category(user_id)
         elif choice == '4':
-            add_income(user_id)
+            application.add_income(user_id)
         elif choice == '5':
-            add_expense(user_id)
+            application.add_expense(user_id)
         elif choice == '6':
-            edit_income(user_id)
+            application.edit_income(user_id)
         elif choice == '7':
-            edit_expense(user_id)
+            application.edit_expense(user_id)
         elif choice == '8':
-            delete_income(user_id)
+            application.delete_income(user_id)
         elif choice == '9':
-            delete_expense(user_id)
+            application.delete_expense(user_id)
         elif choice == '10':
             break
         else:
             print("Invalid choice. Please try again.")
 
 
-def view_expense_by_category(user_id):
-    expense_by_category = application.get_expense_by_category(user_id)
-    print("Expense by Category:")
-    for category, amount in expense_by_category:
-        print(f"{category}: {amount}")
-
-
-def view_all_expenses_category(user_id):
-    # Get all unique categories for the user
-    categories = set(expense[3] for expense in application.get_all_expenses_category(user_id))
-
-    print("Expense Categories:")
-    for i, category in enumerate(categories, start=1):
-        print(f"{i}. {category}")
-
-    # Ask the user to choose a category
-    category_choice = input("Choose a category (enter the corresponding number): ")
-
-    try:
-        category_index = int(category_choice) - 1
-        chosen_category = list(categories)[category_index]
-        view_expenses_for_category(user_id, chosen_category)
-    except (ValueError, IndexError):
-        print("Invalid choice. Please try again.")
-
-
-def view_expenses_for_category(user_id, category):
-    expenses_for_category = application.get_expenses_for_category(user_id, category)
-    print(f"All Expenses for {category}:")
-    for expense in expenses_for_category:
-        print(expense)
-
-
-def add_income(user_id):
-    amount = float(input("Enter the income amount: "))
-    description = input("Enter a description for the income: ")
-    application.add_income(user_id, amount, description)
-    print("Income added successfully!\n")
-
-
-def add_expense(user_id):
-    amount = float(input("Enter the expense amount: "))
-    category = input("Enter the expense category: ")
-    description = input("Enter a description for the expense: ")
-    application.add_expense(user_id, amount, category, description)
-    print("Expense added successfully!\n")
-
-
-def view_all_incomes(user_id):
-    all_incomes = application.get_all_incomes(user_id)
-    print("All Incomes:")
-    for income in all_incomes:
-        print(income)
-
-
-def view_all_expenses(user_id):
-    all_expenses = application.get_all_expenses(user_id)
-    print("All Expenses:")
-    for expense in all_expenses:
-        print(expense)
-
-
-def view_all_expenses_category(user_id):
-    all_expenses_category = application.get_all_expenses_category(user_id)
-    print("All Expenses by Category:")
-    for expense in all_expenses_category:
-        print(expense)
-
-
-def edit_income(user_id):
-    view_all_incomes(user_id)
-
-    expense_id = input("Enter the ID of the income you want to delete: ")
-
-
-def edit_expense(user_id):
-    view_all_expenses(user_id)
-
-    expense_id = input("Enter the ID of the expense you want to edit: ")
-
-
-def delete_income(user_id):
-    view_all_incomes(user_id)
-
-    expense_id = input("Enter the ID of the income you want to delete: ")
-
-
-def delete_expense(user_id):
-    view_all_expenses(user_id)
-
-    expense_id = input("Enter the ID of the expense you want to delete: ")
+# def view_all_expenses_category(user_id):
+#     # Get all unique categories for the user
+#     categories = set(expense[3] for expense in application.get_all_expenses_category(user_id))
+#
+#     print("Expense Categories:")
+#     for i, category in enumerate(categories, start=1):
+#         print(f"{i}. {category}")
+#
+#     # Ask the user to choose a category
+#     category_choice = input("Choose a category (enter the corresponding number): ")
+#
+#     try:
+#         category_index = int(category_choice) - 1
+#         chosen_category = list(categories)[category_index]
+#         view_expenses_for_category(user_id, chosen_category)
+#     except (ValueError, IndexError):
+#         print("Invalid choice. Please try again.")
+#
+#
+# def view_expenses_for_category(user_id, category):
+#     expenses_for_category = application.get_expenses_for_category(user_id, category)
+#     print(f"All Expenses for {category}:")
+#     for expense in expenses_for_category:
+#         print(expense)
+#
+#
+# def view_all_expenses_by_category(user_id):
+#     all_expenses_category = application.get_all_expenses_category(user_id)
+#     print("All Expenses by Category:")
+#     for expense in all_expenses_category:
+#         print(expense)
 
 
 if __name__ == '__main__':
